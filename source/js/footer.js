d60febe139526765aa30b64e00c3c8d7ca2c0c10
小鱼yuzifu <yuzifu@yuzifu.top>
2023-02-12 01:06:09 +0800

    谢谢土土！

---
+++ source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;"> Powered by <a href="https://github.com/ye-tutu/TuHome" target="_blank">TuHome</a> | &copy ' + new Date().getFullYear() + ' 小鱼yuzifu</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
---
+++ source/js/header.js
@@ -0,0 +1,35 @@
+document.write('<!-----------  公共js和css start----------->');
+document.write('' +
+    '<link rel="stylesheet" href="source/css/shubiao.css">\n' +
+    // '<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?8c0854c37b23b617c441acb65529fca1";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>\n' +
+    '<link rel="apple-touch-icon" sizes="76x76" href="source/img/logo.png">\n' +
+    '<link rel="shortcut icon" href="source/img/logo.png" type="image/x-png">\n' +
+    '<link rel="stylesheet" href="source/css/mystyle.css">\n' +
+    '<link rel="stylesheet" href="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.1/css/all.css">\n' +
+    '<link rel="stylesheet" href="//at.alicdn.com/t/font_2037988_kedowpbmdn.css">\n' +
+    '<link rel="stylesheet" href="//at.alicdn.com/t/c/font_3838554_b1uu7chth8n.css">\n' +
+    '');
+
+window.oncontextmenu = function (event) {
+    return false;
+}
+
+// setInterval(function () {
+//     check()
+// }, 100);
+// var check = function () {
+//     function doCheck(a) {
+//         if (("" + a / a)["length"] !== 1 || a % 20 === 0) {
+//             (function () { }
+//             ["constructor"]("debugger")())
+//         } else {
+//             (function () { }
+//             ["constructor"]("debugger")())
+//         }
+//         doCheck(++a)
+//     }
+//     try {
+//         doCheck(0)
+//     } catch (err) { }
+// };
+// check();
